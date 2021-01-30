8735401a909d3152347ba140a65b9bdc1f7b2bfd
humraahsan@gmail.com <humraahsan@Humras-MBP.lan>
2021-01-29 19:09:01 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def test(greetings):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'{greetings}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test('Hello World')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
